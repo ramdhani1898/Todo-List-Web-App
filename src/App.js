24ab01d545405bba61f28fa730190b80a1672fe7
unknown <ramdhanidani74@gmail.com>
2022-06-24 13:59:56 +0700

    update

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import TodoForm from './components/TodoForm'
+// import Toggle from './components/Toggle'
+import './components/style.css'
+
+const App = () => {
+
+    return (
+        <>
+            <div>
+                <TodoForm />
+                {/*<Toggle />*/}
+            </div>
+                    
+        </>
+    )
+}
+
+export default App
---
+++ src/components/TodoList.js
@@ -0,0 +1,29 @@
+import { useContext } from 'react'
+import './style.css'
+import { ThemeContext } from '../context/context'
+
+const TodoList = ({ items, deleteItem, editItem }) => {
+    // DARKMODE
+    const theme = useContext(ThemeContext)
+    const darkMode = theme.state.darkMode
+
+    return(
+        <>
+            {items.map((item) => (
+                <article className="article" key={item.id}>
+                    <div className="article-container" style={{background: darkMode ? "rgb(221, 213, 213)" : "rgb(53, 50, 50)"}}>
+                        <div className="first">
+                            <p className="" style={{color: darkMode ? "#000" :"#fff"}}>{item.title}</p>
+                        </div>
+                        <div className="second">
+                            <button onClick={() => editItem(item.id)}>Edit</button>
+                            <button className="deleteItem" onClick={() => deleteItem(item.id)}>Hapus</button>
+                        </div>
+                    </div>
+                </article>
+            ))}
+        </>
+    )
+}
+
+export default TodoList
